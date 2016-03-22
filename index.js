78e9fbb2506d0fe4e38d8f7ec7354606fc49a26c
neolegolas14 <aliens123456@gmail.com>
2016-03-22 02:44:26 -0700

    Fixed Local Storage of Switches

--- index.js
+++ index.js
@@ -1,15 +1,63 @@
 (function(window){
 window.init = function(){
     console.log('Initializing JS');
-    $('.ui .checkbox').checkbox().first().checkbox({
-      onChecked : function(){
-        window.open('http://go/vivqusoptin','_blank');
-      },
-      onUnchecked : function(){
-        window.open('http://go/vivqusoptout','_blank');
-      }
-    });
     $('.ui.accordion').accordion();
+    $('.infoLink').popup();
+    $('.save').popup();
+    $('.load').popup();
+    $('.clear').popup();
+    var localSwitches;
+    $('.load').on('click',function(event){
+      chrome.storage.sync.get('uiSwitches', function(data) {
+            var localSwitches = [];
+            if(data && data.uiSwitches && data.uiSwitches.length>0){
+                  for(var i=0;i<data.uiSwitches.length;i++){
+                      localSwitches.push(new Switch(data.uiSwitches[i]._name,data.uiSwitches[i]._optInUrl,data.uiSwitches[i]._optOutUrl,data.uiSwitches[i]._defaultState,i));
+                  }
+                  createAllSwitches(localSwitches);
+                  $('.ui.accordion').accordion('open',1);
+            }
+          });
+    });
+    $('.save').on('click',function(event){
+        var self = this;
+        saveSwitches(localSwitches);
+    });
+    $('.clear').on('click',function(event){
+      chrome.storage.sync.set({'uiSwitches' : ''}, function() {
+            // Notify that we cleared.
+            console.log('Cleared');
+          });
+      var newcss = addDisabled($('.clear').attr('class'));
+      $('.clear').attr('class',newcss);
+      $('.switchContainer').empty();
+    });
+    function addDisabled(cssclass){
+      if(cssclass.indexOf('disabled')==-1){
+        cssclass  = cssclass + ' disabled';
+        return cssclass;
+      }
+      return cssclass;
+    }
+    function removeDisabled(cssclass){
+      if(cssclass.indexOf('disabled')!=-1){
+        return cssclass.replace('disabled','');
+      }
+      return cssclass;
+    }
+    chrome.storage.sync.get('uiSwitches', function(data) {
+          if(data && data.uiSwitches && data.uiSwitches.length>0){
+              var newcss = removeDisabled($('.clear').attr('class'));
+              $('.clear').attr('class',newcss);
+          }
+        });
+    $('.clear').popup();
+    $('.browse').on('click',function(){
+        $('#file').click();
+    });
+    $('.browseDiv').on('click',function(){
+        $('#file').click();
+    });
     function onChange(event) {
         var reader = new FileReader();
         reader.onload = onReaderLoad;
@@ -21,7 +69,6 @@ window.init = function(){
         var switchContainerEl = $('.switchContainer');
         var switchStartHtml = "<div class='switch'>";
         var switchEndHtml = "</div><div class='ui divider'></div>";
-        this.switches = switches;
 
         for(var i=0;i<switches.length;i++){
             var labelHtml = "<label class='switch-label'>"+switches[i].getName()+"</label>";
@@ -31,13 +78,31 @@ window.init = function(){
             $('.switch_'+switches[i].getIndex()).checkbox({
               onChecked : function(){
                 var self = this;
+                switches[$(this).attr('data-index')].setDefaultState('optin');
                 window.open(switches[$(this).attr('data-index')].getOptInUrl(),'_blank');
+                saveSwitches(switches);
               },
               onUnchecked : function(){
+                var self= this;
+                switches[$(this).attr('data-index')].setDefaultState('optout');
                 window.open(switches[$(this).attr('data-index')].getOptOutUrl(),'_blank');
+                saveSwitches(switches);
               }
             });
         }
+    }
+    function saveSwitches(switches){
+      chrome.storage.sync.set({'uiSwitches' : switches}, function() {
+            // Notify that we saved.
+            console.log('Saved');
+          });
+      chrome.storage.sync.get('uiSwitches', function(data) {
+                if(data && data.uiSwitches && data.uiSwitches.length>0){
+                    console.log('here');
+                    var newcss = removeDisabled($('.clear').attr('class'));
+                    $('.clear').attr('class',newcss);
+                }
+              });
     }
     function onReaderLoad(event){
         var switches = [];
@@ -45,8 +110,7 @@ window.init = function(){
         for(var i=0;i<obj.switches.length;i++){
           switches.push(new Switch(obj.switches[i].name,obj.switches[i].optInUrl,obj.switches[i].optOutUrl,obj.switches[i].defaultState,i));
         }
-        createAllSwitches(switches);
-        $('.ui.accordion').accordion('open',1);
+        localSwitches = switches;
     }
 
     $("#file").on('change', onChange);
